88d403c9f17ca6bce0bbc87013cb4ed7f98520c8
Monica <magrawal@mimeo.com>
2024-06-19 17:34:43 +0530

    updated packer build command

--- images.CI/linux-and-win/build-image-mimeo.ps1
+++ images.CI/linux-and-win/build-image-mimeo.ps1
@@ -32,21 +32,18 @@ packer init $TemplatePath
 Write-Host "Validate packer template"
 packer validate -syntax-only $TemplatePath
 
-Write-Host "Validate $ImageTemplateName "
-packer validate $TemplatePath
-
 Write-Host "Build $ImageTemplateName VM"
-packer build    -var "client_id=$ClientId" `
-                -var "client_secret=$ClientSecret" `
-                -var "install_password=$InstallPassword" `
-                -var "location=$Location" `
-                -var "managed_image_name=$ImageName" `
-                -var "managed_image_resource_group_name=$ImageResourceGroupName" `
-                -var "subscription_id=$SubscriptionId" `
-                -var "temp_resource_group_name=$TempResourceGroupName" `
-                -var "tenant_id=$TenantId" `
-                -var "virtual_network_name=$VirtualNetworkName" `
-                -var "virtual_network_resource_group_name=$VirtualNetworkRG" `
-                -var "virtual_network_subnet_name=$VirtualNetworkSubnet" `
-                -var "vm_size=$VMSize"
+packer build    -var client_id=$ClientId `
+                -var client_secret=$ClientSecret `
+                -var install_password=$InstallPassword `
+                -var location=$Location `
+                -var managed_image_name=$ImageName `
+                -var managed_image_resource_group_name=$ImageResourceGroupName `
+                -var subscription_id=$SubscriptionId `
+                -var temp_resource_group_name=$TempResourceGroupName `
+                -var tenant_id=$TenantId `
+                -var virtual_network_name=$VirtualNetworkName `
+                -var virtual_network_resource_group_name=$VirtualNetworkRG `
+                -var virtual_network_subnet_name=$VirtualNetworkSubnet `
+                -var vm_size=$VMSize `
                 $TemplatePath
