6eb806cb40a251cd7992084550ba8d4faed5c75f
Monica <90908113+monicav15@users.noreply.github.com>
2025-03-18 21:17:24 +0530

    Update install-kotlin.sh

--- images/ubuntu/scripts/build/install-kotlin.sh
+++ images/ubuntu/scripts/build/install-kotlin.sh
@@ -1,15 +1,10 @@
-#!/bin/bash -e
-################################################################################
-##  File:  install-kotlin.sh
-##  Desc:  Install Kotlin
-##  Supply chain security: Kotlin - checksum validation
-################################################################################
-
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
 
 KOTLIN_ROOT="/usr/share"
-download_url=$(resolve_github_release_asset_url "JetBrains/kotlin" "contains(\"kotlin-compiler\") and endswith(\".zip\")" "latest")
+
+# Pin to Kotlin 1.6.x for kotlin-dce-js compatibility
+download_url=$(resolve_github_release_asset_url "JetBrains/kotlin" "contains(\"kotlin-compiler-1.6\") and endswith(\".zip\")" "latest")
 archive_path=$(download_with_retry "$download_url")
 
 # Supply chain security - Kotlin
@@ -17,8 +12,11 @@ kotlin_hash_file=$(download_with_retry "${download_url}.sha256")
 kotlin_hash=$(cat "$kotlin_hash_file")
 use_checksum_comparison "$archive_path" "$kotlin_hash"
 
+# Extract and install
 unzip -qq "$archive_path" -d $KOTLIN_ROOT
 rm $KOTLIN_ROOT/kotlinc/bin/*.bat
 ln -sf $KOTLIN_ROOT/kotlinc/bin/* /usr/bin
 
+# Run tests to ensure all binaries work, including kotlin-dce-js
 invoke_tests "Tools" "Kotlin"
+
